c6c527b974cde07b2e88ce3b4a4c7a60f4540657
greengrape <andy@spdg.net>
2020-04-05 20:36:45 +0300

    wip

--- cd.py
+++
@@ -1,8 +0,0 @@
-import requests_html, os
-from selenium import webdriver
-
-link = 'https://author.today/work/33438'
-
-gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))
-binary = webdriver.firefox.firefox_binary.FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
-driver = webdriver.Firefox(firefox_binary=binary, executable_path=gecko+'.exe')
